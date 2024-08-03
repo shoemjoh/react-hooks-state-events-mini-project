6d1c13cccb5ed6faa70133e0802732ee8121a1f6
John <shoemjoh@gmail.com>
2024-08-03 15:15:18 -0400

    working lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
@@ -8,12 +8,45 @@ console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [tasks, setTasks] = useState(TASKS);
+  const [filteredTasks, setFilteredTasks] = useState(TASKS);
+
+  const handleCategoryChange = (category) => {
+    if (category === "All") {
+      setFilteredTasks(tasks);
+    } else {
+      const updatedTasks = tasks.filter(task => task.category === category);
+      setFilteredTasks(updatedTasks);
+    }
+  };
+
+  const handleTaskFormSubmit = (newTask) => {
+    const updatedTasks = [...tasks, newTask];
+    setTasks(updatedTasks);
+    setFilteredTasks(updatedTasks);
+  };
+
+  const handleDelete = (taskText) => {
+    const updatedTasks = tasks.filter(task => task.text !== taskText);
+    setTasks(updatedTasks);
+    setFilteredTasks(updatedTasks);
+  };
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter
+        categories={CATEGORIES}
+        onCategoryChange={handleCategoryChange}
+      />
+      <NewTaskForm
+        categories={CATEGORIES}
+        onTaskFormSubmit={handleTaskFormSubmit}
+      />
+      <TaskList
+        tasks={filteredTasks}
+        onDelete={handleDelete}
+      />
     </div>
   );
 }
--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CategoryFilter() {
+function CategoryFilter({ categories, onCategoryChange }) {
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  const handleClick = (category) => {
+    console.log("New category clicked")
+    setSelectedCategory(category);
+    onCategoryChange(category);
+
+  }
+
   return (
     <div className="categories">
+      {categories.map(category => (
+        <button
+          key={category}
+          className={selectedCategory === category ? "selected" : ""}
+          onClick={() => handleClick(category)}
+        >
+          {category}
+        </button>
+      ))}
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+
     </div>
   );
 }
